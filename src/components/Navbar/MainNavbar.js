d7a582ec148a18653ec4bdc3c05d722334f2ee63
El-Hassan Mohsen Farid <hassan.m.farid@gmail.com>
2020-03-28 01:30:48 +0100

    Added themes to the site

--- src/components/Navbar/MainNavbar.js
+++ src/components/Navbar/MainNavbar.js
@@ -1,24 +1,47 @@
 // Imports
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../../assests/imgs/navbar/logostroke.png';
+
+// CSS
+import './MainNavbar.css';
 
 // The navigation bar
 const mainNavbar = props => (
-	<header>
-
-		<div className="navLogo">
-			<h1> Eventee </h1>
-		</div>
-
-		<nav className="navItems">
-			<ul>
-				<li><NavLink to="/auth"> Profile </NavLink></li>
-				<li><NavLink to="/events"> Events </NavLink></li>
-				<li><NavLink to="/bookings"> Bookings </NavLink></li>
+	<div>
+		<nav className="navbar">
+			<ul className="navItems">
+				<li className="navItem profile">
+					<NavLink to="/profile">Profile</NavLink>
+				</li>
+				<li className="navItem">
+					<NavLink to="/events">Events</NavLink>
+				</li>
+				<li className="navItem">
+					<NavLink to="/bookings">Bookings</NavLink>
+				</li>
+				<li className="navItem hasDropdown">
+					<a href="#">Theme</a>
+					<ul className="dropdown">
+						<li className="dropdownItem"> <a id="dark" href="#">Dark</a> </li>
+						<li className="dropdownItem"> <a id="light" href="#">Light</a> </li>
+						<li className="dropdownItem"> <a id="default" href="#">Default</a> </li>
+					</ul>
+				</li>
+				<li className="navItem login">
+					<NavLink to="/auth">Login</NavLink>
+				</li>
 			</ul>
 		</nav>
-
-	</header>
+		
+		<header>
+			<div className="navLogo">
+				<img src={logo} alt={'Logo'} className="logo" height="250" width="250"/>
+				<h1>Eventee | Events Manager</h1>
+				<p>React | Node | Express | GraphQL | MongoDB</p>
+			</div>
+		</header>
+	</div>
 );
 
 // Exports
